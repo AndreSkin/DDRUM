1b2038285f9bf800d30a995659433445c6d1aeb1
Andrea <80905987+AndreSkin@users.noreply.github.com>
2024-02-20 10:28:50 +0100

    Trie implementation and accessibility

--- Interface/static/front.js
+++ Interface/static/front.js
@@ -1,68 +1,118 @@
 var server = "http://localhost:5000";
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
+    }
+
+    search(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return node != null && node.isEndOfWord;
+    }
+
+    getWordsWithPrefix(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+            if (!node.children[char]) {
+                return [];
+            }
+            node = node.children[char];
+        }
+        return this._getAllWordsFromNode(node, prefix);
+    }
+
+    _getAllWordsFromNode(node, prefix) {
+        let words = [];
+        if (node.isEndOfWord) {
+            words.push(prefix);
+        }
+        for (let char in node.children) {
+            words = words.concat(this._getAllWordsFromNode(node.children[char], prefix + char));
+        }
+        return words;
+    }
+}
+
+
 $(document).ready(function() {
-    // Arrays to store drug names and condition names
-    var drugNames = [];
-    var conditionNames = [];
+    // Trie to store drug names and condition names
+    var drugTrie = new Trie();
+    var conditionTrie = new Trie();
     
-    // Function to fetch drug names
+    // Function to fetch drug names and populate the trie
     $.getJSON(server+"/drugs", function(data) {
         $.each(data.results.bindings, function(index, value) {
             let drugName = value.drugName.value.charAt(0).toUpperCase() + value.drugName.value.slice(1); 
-            drugNames.push(drugName);
+            drugTrie.insert(drugName.toLowerCase());
         });
     });
 
-    // Function to fetch condition names
+    // Function to fetch condition names and populate the trie
     $.getJSON(server+'/conditions', function(data) {
         $.each(data.results.bindings, function(index, value) {
             let conditionName = value.conditionName.value.charAt(0).toUpperCase() + value.conditionName.value.slice(1); 
-            conditionNames.push(conditionName);
+            conditionTrie.insert(conditionName.toLowerCase());
         });
     });
 
-    // Function to display drug suggestions
+    // Function to display suggestions
+    function displaySuggestions(input, trie, suggestionsContainer) {
+        suggestionsContainer.html('');
+        if (input.length >= 1) {
+            $(".scrollable-suggestions").css("border", "1px solid #ced4da");
+            var suggestions = trie.getWordsWithPrefix(input.toLowerCase());
+            $.each(suggestions, function(index, value) {
+                suggestionsContainer.append('<div class="suggestion">' + value + '</div>');
+            });
+        }
+        else {
+            $(".scrollable-suggestions").css("border", "none");
+        }
+    }
+
+    // Function to handle input for drug search
     $('#drugSearch').on('input', function() {
-        var userInput = $(this).val().toLowerCase();
-        $('#drugSuggestions').html('');
-        if (userInput.length >= 1) {
-            $(".scrollable-suggestions").css("border", "1px solid #ced4da");
-            var suggestions = drugNames.filter(function(drug) {
-                return drug.toLowerCase().startsWith(userInput);
-            });
-            $.each(suggestions, function(index, value) {
-                $('#drugSuggestions').append('<div class="suggestion">' + value + '</div>');
-            });
-        }
-        else {
-            $(".scrollable-suggestions").css("border", "none");
-        }
+        var userInput = $(this).val();
+        displaySuggestions(userInput, drugTrie, $('#drugSuggestions'));
     });
 
-    // Function to display condition suggestions
+    // Function to handle input for condition search
     $('#conditionSearch').on('input', function() {
-        var userInput = $(this).val().toLowerCase();
-        $('#conditionSuggestions').html('');
-        if (userInput.length >= 1) {
-            $(".scrollable-suggestions").css("border", "1px solid #ced4da");
-            var suggestions = conditionNames.filter(function(condition) {
-                return condition.toLowerCase().startsWith(userInput);
-            });
-            $.each(suggestions, function(index, value) {
-                $('#conditionSuggestions').append('<div class="suggestion">' + value + '</div>');
-            });
-        }
-        else {
-            $(".scrollable-suggestions").css("border", "none");
-        }
+        var userInput = $(this).val();
+        displaySuggestions(userInput, conditionTrie, $('#conditionSuggestions'));
     });
 
-    // Clicking on drug suggestion
+    // Clicking on suggestion for drug search
     $('#drugSuggestions').on('click', '.suggestion', function() {
         $('#drugSearch').val($(this).text());
         $('#drugSuggestions').html('');
     });
 
-    // Clicking on condition suggestion
+    // Clicking on suggestion for condition search
     $('#conditionSuggestions').on('click', '.suggestion', function() {
         $(".scrollable-suggestions").css("border", "none");
         $('#conditionSearch').val($(this).text());
@@ -84,14 +134,14 @@ $(document).ready(function() {
     });
 });
 
+
 function submitForms() {
     $(".scrollable-suggestions").css("border", "none");
     $('#conditionSuggestions').html('');
     $('#drugSuggestions').html('');
-    $("#results").empty();
+    $("#results").empty().attr("aria-busy", "true").attr("aria-live", "polite").attr("aria-atomic", "true");
     $("#error").empty();
     var formData = {};
-    var isValid = true;
 
     // Get the selected form
     var selectedValue = $("#dropdownMenu").val();
@@ -120,7 +170,6 @@ function submitForms() {
     } else {
         formData["num_res"] = numres;
     }
-    // console.log(JSON.stringify(formData));
 
     // Send data as a POST request
     fetch(server+'/submit', {
@@ -139,10 +188,10 @@ function submitForms() {
     .then(response => {
         var html = "<br>";
         if (response.head && response.results && response.results.bindings) {
-            html += "<table class='table table-striped table-vertical-lines'><thead><tr>";
+            html += "<table class='table table-striped table-vertical-lines' role='table'><thead><tr>";
             var vars = response.head.vars;
             vars.forEach(function(variable) {
-                html += `<th>` + variable.charAt(0).toUpperCase() + variable.slice(1) + "</th>";
+                html += `<th scope='col'>` + variable.charAt(0).toUpperCase() + variable.slice(1) + "</th>";
             });
             html += "</tr></thead><tbody>";
             var bindings = response.results.bindings;
@@ -170,10 +219,9 @@ function submitForms() {
         } else {
             html += "<p>Unexpected response format.</p>";
         }
-        $("#results").append(html);
+        $("#results").empty().append(html).removeAttr("aria-busy").removeAttr("aria-live").removeAttr("aria-atomic");
     })
     .catch(error => {
         console.error('There was a problem with your fetch operation:', error);
     });
 }
-
